24ec786502cab0dbeac831658c681ce44c4ceb47
martinrysanek <33803015+martinrysanek@users.noreply.github.com>
2023-09-10 20:01:58 +0200

    Add files via upload

--- main_wafaq.py
+++ main_wafaq.py
@@ -24,6 +24,26 @@ class CustomFormatter(logging.Formatter):
         # custom_format = f"[{timestamp}] [{record.levelname}] {record.name}: {record.getMessage()}"
         custom_format = f"[{timestamp}] [{record.levelname}]: {record.getMessage()} <BR>"
         return custom_format
+    
+def wa_login():
+    global authenticator
+    global assistant
+    global assistant_id
+    global session_id
+    global api_key
+    global wa_url
+
+    if not authenticator:    
+        logger.debug("wa_login() new wa session start")
+        authenticator = IAMAuthenticator(api_key)
+        assistant = AssistantV2(
+            version='2021-11-27',
+            authenticator=authenticator)
+        assistant.set_service_url(wa_url)
+        session = assistant.create_session(assistant_id).get_result()
+        session_id=session['session_id']
+        logger.debug("wa_login() new wa session " + session_id)
+        
 
 # set up root route
 @app.route("/log", methods=['GET'])
@@ -38,7 +58,11 @@ def log():
 @app.route("/query", methods=['POST'])
 def query_api():
   try:
-      global logger      
+      global logger  
+      global assistant_id
+      global session_id  
+      global assistant
+      
       logger.debug("/query POST")
       request_data = request.get_json()
       if 'query' not in request_data:
@@ -50,18 +74,51 @@ def query_api():
           return jsonify({"error": "Wrong 'query' parameter type"}), 400
       else:
           logger.info("Query: parameter: " + query)
-      response_data = [
-          {
-           'intent': 'intent string',
-           'text':  'text string',
-           'confidence' : 0.6
-          },
-          {
-           'intent': 'intent string2',
-           'text':  'text string2',
-           'confidence' : 0.7
-          }
-      ]
+          
+      #Create WA session if it is not opened yet
+      wa_login()
+      
+      #Get Intents for Query
+      result = assistant.message(
+        assistant_id=assistant_id,
+        session_id=session_id,
+        input={
+            'message_type': 'text',
+            'text': query,
+            "options": {
+            "alternate_intents": True        
+            }
+        }
+      )
+      response = result.get_result()
+      
+      if result.status_code == 200 and 'intents' in response["output"]:
+          response_data = []
+          intents = response["output"]['intents']
+          for intent in intents:
+              logger.info("Query: intent " + intent['intent'])
+              new_item = {
+                  'intent': intent['intent'],
+                  'text':  'text',
+                  'confidence' : intent['confidence']
+              }
+              response_data.append(new_item)
+      else:
+          logger.error("Query: Wa reponded with error")
+          return jsonify({"error": "Wa reponded with error"}), 400    
+      
+      # response_data = [
+      #     {
+      #      'intent': 'intent string',
+      #      'text':  'text string',
+      #      'confidence' : 0.6
+      #     },
+      #     {
+      #      'intent': 'intent string2',
+      #      'text':  'text string2',
+      #      'confidence' : 0.7
+      #     }
+      # ]
       logger.debug("/query return")
       return jsonify(response_data)
   except Exception as e:
@@ -99,6 +156,9 @@ logger.debug("WA_URL = " + wa_url)
 # Get assistant_id
 assistant_id = os.getenv('ASSISTANT_ID', 'None')
 logger.debug("ASSISTANT_ID = " + assistant_id)
+
+authenticator = None
+assistant = None
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=int(port))
