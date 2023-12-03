9d056a3ecaa4e13c0daee76fa8fd44fc7f0a68f8
martinrysanek <33803015+martinrysanek@users.noreply.github.com>
2023-12-03 16:26:38 +0100

    Add files via upload

--- main_wafaq.py
+++ main_wafaq.py
@@ -257,9 +257,9 @@ def query_api():
                                 words = intent_text_str.split()
                                 capitalized_words = [
                                     word.capitalize() for word in words]
-                                for i in range(len(word)):
-                                    if word[i].lower() == 'ibm':
-                                        word[i] = 'IBM'                                  
+                                for i in range(len(words)):
+                                    if words[i].lower() == 'ibm':
+                                        words[i] = 'IBM'                                  
                                 intent_text_str = " ".join(capitalized_words)
 
                             new_item = {
